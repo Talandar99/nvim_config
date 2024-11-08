103d6ea649ff394a6689a7bc43257afeb14ea6e0
talandar <talandar66@protonmail.com>
2024-11-08 23:52:51 +0100

    feat added some autocommands

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -23,3 +23,52 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	end
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
+
+--automaticly add debuger shortkey if file is rust file
+vim.api.nvim_create_autocmd("BufRead", {
+	pattern = "*.rs",
+	callback = function()
+		vim.keymap.set("n", "<F5>", "<cmd>RustDebuggables<CR>")
+	end,
+})
+--automaticly add debuger shortkey if file is python file
+vim.api.nvim_create_autocmd("BufRead", {
+	pattern = "*.py",
+	callback = function()
+		vim.keymap.set("n", "<F5>", function()
+			require("dap").continue()
+		end)
+	end,
+})
+--automaticly format file using ClangFormat if it's C file
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*.c",
+	callback = function()
+		vim.api.nvim_command("ClangFormat")
+	end,
+})
+
+--automaticly disable indent_blankline if .org
+--automaticly enable conceallevel if .org
+vim.api.nvim_create_autocmd("BufRead", {
+	pattern = "*.org",
+	callback = function()
+		vim.api.nvim_command("IndentBlanklineDisable")
+		vim.opt.conceallevel = 2
+	end,
+})
+
+--automaticly enable conceallevel if .md
+vim.api.nvim_create_autocmd("BufRead", {
+	pattern = "*.md",
+	callback = function()
+		vim.opt.conceallevel = 2
+	end,
+})
+--custom highlight for zenscript
+vim.api.nvim_create_autocmd("BufRead", {
+	pattern = "*.zs",
+	callback = function()
+		vim.api.nvim_command("set filetype=ts")
+	end,
+})
